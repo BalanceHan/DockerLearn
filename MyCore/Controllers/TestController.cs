86378ab4e8d05589fe6b2f123dfe0ebc2f264227
Balance Han <heanhang@outlook.com>
2018-05-06 16:05:54 +0800

    docker自动构建测试

--- MyCore/Controllers/TestController.cs
+++ MyCore/Controllers/TestController.cs
@@ -13,34 +13,9 @@ namespace MyCore.Controllers
     {
         // GET: api/<controller>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Ok("test");
         }
     }
 }
